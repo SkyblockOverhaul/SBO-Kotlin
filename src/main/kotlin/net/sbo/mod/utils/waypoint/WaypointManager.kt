1ae2d152d3dc6c923a2ec4d0e2d8f8ba8d006586
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2025-09-28 17:41:17 +0200

    Refine guess waypoint removal logic
    Updated the filter criteria for removing guess waypoints to use a stricter distance threshold and additional position checks. This improves accuracy in identifying waypoints to remove, especially in multi-burrow scenarios.

--- src/main/kotlin/net/sbo/mod/utils/waypoint/WaypointManager.kt
+++ src/main/kotlin/net/sbo/mod/utils/waypoint/WaypointManager.kt
@@ -80,8 +80,13 @@ object WaypointManager {
             val inqWps = getWaypointsOfType("inq")
 
             if (Diana.dianaMultiBurrowGuess) {
+                val posP = SboVec(playerPos.x, playerPos.y, playerPos.z).roundLocationToBlock()
                 val guessesToRemove = getGuessWaypoints()
-                    .filter { waypoint -> waypoint.distanceToPlayer() < 5.0 || waypoint.pos.y <= 62.0 }
+                    .filter { waypoint ->
+                        waypoint.distanceToPlayer() < 2.0 ||
+                        waypoint.pos.y <= 62.0 ||
+                        (waypoint.pos.x == posP.x && waypoint.pos.z == posP.z && waypoint.pos.y <= posP.y)
+                    }
                     .toList()
 
                 guessesToRemove.forEach { waypoint ->
