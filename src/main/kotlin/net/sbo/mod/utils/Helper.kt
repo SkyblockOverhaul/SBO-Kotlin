e8900376cea75e1bba30d94177bf0c8943aa17bd
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-11-12 18:48:56 +0300

    Fix zero coins shard price
    Hypixel Bazaar API response has shard IDs like this:
    
    SHARD_SPHINX
    SHARD_MINATOUR
    
    etc., while the mod was trying to access:
    
    SPHINX_SHARD
    MINATOUR_SHARD
    
    which returned 0 coins price which in turn was causing all shards in profit display to display zero coins.
    
    There was already an existing workaround of converting sb id CHIMERA to ENCHANTMENT_ULTIMATE_CHIMERA_1, so I extended it to move the _SHARD at the end to the start instead to fix the issue.

--- src/main/kotlin/net/sbo/mod/utils/Helper.kt
+++ src/main/kotlin/net/sbo/mod/utils/Helper.kt
@@ -497,7 +497,7 @@ object Helper {
 
 
     fun getItemPrice(sbId: String, amount: Int = 1): Long {
-        val id = if (sbId == "CHIMERA") "ENCHANTMENT_ULTIMATE_CHIMERA_1" else sbId
+        val id = if (sbId == "CHIMERA") "ENCHANTMENT_ULTIMATE_CHIMERA_1" else if (sbId.endsWith("_SHARD")) "${sbId.substringAfterLast('_')}_${sbId.substringBeforeLast('_')}" else sbId
         var ahPrice = priceDataAh[id]?.toDouble() ?: 0.0
         if (npcSellValueMap.containsKey(id)) {
             val npcPrice = npcSellValueMap[id]?.toDouble() ?: 0.0
