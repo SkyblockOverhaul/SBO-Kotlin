0ac95133b4155091433320f266a8596bd3b7b156
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2025-11-30 23:32:42 +0100

    Update SboEvent documentation with event package list
    Expanded the documentation for the SboEvent annotation to specify the organization of event types into their respective packages. This provides clearer guidance for developers on where to find event types.

--- src/main/kotlin/net/sbo/mod/utils/events/annotations/SboEvent.kt
+++ src/main/kotlin/net/sbo/mod/utils/events/annotations/SboEvent.kt
@@ -9,7 +9,13 @@ package net.sbo.mod.utils.events.annotations
  *
  * The annotated function:
  * - Must have exactly one parameter, representing the event type.
- *   All event types can be found in [net.sbo.mod.utils.events.impl].
+ * Events are organized in the following packages:
+ * - [net.sbo.mod.utils.events.impl.entity]
+ * - [net.sbo.mod.utils.events.impl.game]
+ * - [net.sbo.mod.utils.events.impl.guis]
+ * - [net.sbo.mod.utils.events.impl.packets]
+ * - [net.sbo.mod.utils.events.impl.partyfinder]
+ * - [net.sbo.mod.utils.events.impl.render]
  * - Must be defined inside an `object` singleton or a `companion object`.
  *
  * The function is automatically registered with the event bus at compile time
@@ -19,7 +25,7 @@ package net.sbo.mod.utils.events.annotations
  * ```kotlin
  * @SboEvent
  * fun onSomeEvent(event: SomeEvent) {
- *     // handle the event here
+ * // handle the event here
  * }
  * ```
  *
