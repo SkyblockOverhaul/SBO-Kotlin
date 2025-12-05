c29cd4de5a8567f50d8e336f87510cc8aa5e8f83
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2025-12-05 17:32:54 +0100

    Add 1.21.10-fabric version and update dependencies
    Introduces the 1.21.10-fabric version to the project, updates Kotlin and multiversion plugin versions, and adjusts preprocessing hierarchy to include the new version. Also adds a .gitignore for the new version directory.

--- root.gradle.kts
+++ root.gradle.kts
@@ -3,7 +3,9 @@ plugins {
 }
 
 preprocess {
-    "1.21.7-fabric"(1_21_08, "yarn") {
-        "1.21.5-fabric"(1_21_05, "yarn")
+    "1.21.10-fabric"(1_21_10, "yarn") {
+        "1.21.7-fabric"(1_21_08, "yarn") {
+            "1.21.5-fabric"(1_21_05, "yarn")
+        }
     }
 }
