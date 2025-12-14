3654965255175e08e46ae0ad6d9610da38f38670
ugo635 <ugolechien58901@gmail.com>
2025-12-14 15:54:39 +0100

    New !since

--- src/main/kotlin/net/sbo/mod/general/PartyCommands.kt
+++ src/main/kotlin/net/sbo/mod/general/PartyCommands.kt
@@ -397,6 +397,24 @@ object PartyCommands {
                         "lschim", "chimls", "lschimera", "chimerals", "lsbook", "bookls", "lootsharechim" -> sleep(200) {
                             Chat.command("pc Inqs since lootshare chim: ${sboData.inqsSinceLsChim}")
                         }
+                        "kings", "king" -> sleep(200) {
+                            Chat.command("pc Mobs since king: ${sboData.mobsSinceKing}")
+                        }
+                        "manti" -> sleep(200) {
+                            Chat.command("pc Mobs since manti: ${sboData.mobsSinceManti}")
+                        }
+                        "core", "cores" -> sleep(200) {
+                            Chat.command("Mantis since core: ${sboData.mantiSinceCore}")
+                        }
+                        "wool", "wools" -> sleep(200) {
+                            Chat.command("Mantis since wool: ${sboData.kingSinceLsWool}")
+                        }
+                        "corels", "lscore" -> sleep(200) {
+                            Chat.command("Mantis since lootshare core: ${sboData.mantiSinceLsCore}")
+                        }
+                        "woolls", "lswool" -> sleep(200) {
+                            Chat.command("Mantis since lootshare wool: ${sboData.kingSinceLsWool}")
+                        }
                     }
                     if (secondArg == null) {
                         sleep(200) {
